0e861d081da68e6e675f11685dd5aa46dd0b6a42
Justin <justinanderson101@gmail.com>
2021-03-10 22:53:41 +0000

    work in progress

--- play.js
+++ play.js
@@ -1,5 +1,23 @@
 const net = require('net');
+// const { stdin } = require('node:process');
 const { connect } = require('./client.js')
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+setupInput();
+
