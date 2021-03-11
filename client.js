ee89ef2c96a71ee4f7667393ed7d9678502267a4
Justin <justinanderson101@gmail.com>
2021-03-11 02:34:34 +0000

    finished snaked

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 const net = require('net');
+const { IP, PORT } = require('./constants.js');
 
 /**
  * Establishes connection with the game server
  */
 const connect = function (){
   const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
+    host: IP,
+    port: PORT
   });
   conn.on('data', function (data) {
     console.log(data)
